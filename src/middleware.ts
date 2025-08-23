8afdd53900c138fc647fc4845405d622b4aa4bdf
Deepali <joysingal247@gmail.com>
2025-08-23 15:19:22 +0530

    changes in middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,9 @@
-// Middleware is meant to be fast; move full user creation to API route
+// src/middleware.ts
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-import { clerkMiddleware, auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-
-export default clerkMiddleware(async () => {
-  const { userId } = await auth();
-
-  if (!userId) {
-    // User is not logged in, just continue
-    return NextResponse.next();
-  }
-
-  return NextResponse.next();
-});
+export default clerkMiddleware();
 
 export const config = {
+  // Match all routes except static files & public assets
   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)", "/api/:path*"],
 };
